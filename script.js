794fe8ddd4f47cd3129d819495f007d929fc730a
Mayur Ghodaskar <97151293+Mayur244@users.noreply.github.com>
2025-10-28 16:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freq = new Map();
+
+	for(let ch of str){
+		freq.set(ch, (freq.get(ch) || 0) + 1);
+	}
+
+	for(let [ch, count] of freq){
+		if(count === 1){
+			return ch;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
